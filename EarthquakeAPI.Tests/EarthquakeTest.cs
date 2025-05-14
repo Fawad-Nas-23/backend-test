ba4d5d47d12e6e679fabdf21fe17afa731021130
Fawad Nassery <eaa23fana@students.eaaa.dk>
2025-05-14 23:42:42 +0200

    main up to date

--- EarthquakeAPI.Tests/EarthquakeTest.cs
+++ EarthquakeAPI.Tests/EarthquakeTest.cs
@@ -28,7 +28,7 @@ public sealed class EarthquakeTest
     }
 
     [TestMethod]
-    public void UpdateLocationInSameHemisphere()
+    public void TestUpdateLocationInSameHemisphere()
     {
         var repo = new PersonRepository();
         var person = new Person
@@ -43,7 +43,7 @@ public sealed class EarthquakeTest
 
         repo.AddPerson(person);
 
-        var newLocation = new EarthquakeAPI.Models.Location { latitude = 20.0, longitude = 50.0 }; 
+        var newLocation = new EarthquakeAPI.Models.Location { latitude = 20.0, longitude = 50.0 };
 
         repo.UpdateLocation(person.firstName, person.lastName, newLocation);
 
@@ -55,7 +55,7 @@ public sealed class EarthquakeTest
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
-    public void UpdateLocationInDifferentHemisphere()
+    public void TestUpdateLocationInDifferentHemisphere()
     {
         var repo = new PersonRepository();
         var person = new Person
@@ -67,7 +67,7 @@ public sealed class EarthquakeTest
             isAlive = true,
             location = new EarthquakeAPI.Models.Location { latitude = 15.0, longitude = 45.0 }
         };
-         
+
         repo.AddPerson(person);
 
         var newLocation = new EarthquakeAPI.Models.Location { latitude = -20.0, longitude = -50.0 };
@@ -76,7 +76,71 @@ public sealed class EarthquakeTest
     }
 
     [TestMethod]
-    public void TestSearchByLastName()
+    public void TestGetAllSurvivorsWithLocations()
+    {
+        var repo = new PersonRepository();
+
+        var alivePerson = new Person
+        {
+            firstName = "Anna",
+            lastName = "Larsen",
+            age = 28,
+            gender = "female",
+            isAlive = true,
+            location = new EarthquakeAPI.Models.Location { latitude = 10.0, longitude = 20.0 }
+        };
+
+        var deceasedPerson = new Person
+        {
+            firstName = "Bo",
+            lastName = "Larsen",
+            age = 50,
+            gender = "male",
+            isAlive = false,
+            location = new EarthquakeAPI.Models.Location { latitude = 15.0, longitude = 25.0 }
+        };
+
+        repo.AddPerson(alivePerson);
+        repo.AddPerson(deceasedPerson);
+
+        var survivors = repo.GetAllSurvivors();
+
+        Assert.AreEqual(1, survivors.Count);
+        Assert.AreEqual("Anna", survivors[0].firstName);
+        Assert.AreEqual(10.0, survivors[0].location.latitude);
+        Assert.AreEqual(20.0, survivors[0].location.longitude);
+    }
+
+    [TestMethod]
+    public void TestGetSurvivorPercantage()
+    {
+        var repo = new PersonRepository();
+
+        var person1 = new Person
+        {
+            firstName = "Anna",
+            lastName = "Larsen",
+            isAlive = true
+        };
+
+        var person2 = new Person
+        {
+            firstName = "Bo",
+            lastName = "Larsen",
+            isAlive = false
+        };
+
+        repo.AddPerson(person1);
+        repo.AddPerson(person2);
+
+
+        double percentage = repo.GetSurvivorPercentage();
+
+        Assert.AreEqual(50, percentage);
+    }
+
+    [TestMethod]
+    public void TestSearchByLasName()
     {
         var repo = new PersonRepository();
         var person1 = new Person
@@ -97,6 +161,7 @@ public sealed class EarthquakeTest
         var result = repo.SearchByLastName("Larsen");
         Assert.AreEqual(2, result.Count);
 
+    }
 
 
 }
