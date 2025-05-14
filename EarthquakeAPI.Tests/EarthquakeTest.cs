b4d2c323ec8ed09c018d41318dd497019e0bbfc2
Fawad Nassery <eaa23fana@students.eaaa.dk>
2025-05-14 18:44:43 +0200

    Add failing test for AddPerson

---
+++ EarthquakeAPI.Tests/EarthquakeTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using System;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+
+namespace EarthquakeAPI.Tests;
+
+[TestClass]
+public sealed class EarthquakeTest
+{
+    [TestMethod]
+    public void AddPerson()
+    {
+        var repository = new PersonRepository();
+        var person = new Person
+        {
+            firstName = "John",
+            lastName = "Doe",
+            age = 30,
+            gender = "male",
+            isAlive = true,
+            location = new Location { latitude = 10.0, longitude = 20.0 }
+        };
+
+        repository.AddPerson(person);
+        var result = repository.GetAllPeople().FirstOrDefault();
+        Assert.AreEqual("John", result.firstName);
+    }
+}
